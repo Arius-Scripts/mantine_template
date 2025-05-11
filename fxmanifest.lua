984dbb103ca124b9fb34c082a85df208d8af5884
Arius <70983185+Arius-Scripts@users.noreply.github.com>
2025-05-11 19:01:37 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,27 +3,18 @@ use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 game "gta5"
 
-name 'ox_lib'
-author 'Overextended'
-version '3.30.6'
-license 'LGPL-3.0-or-later'
-repository 'https://github.com/overextended/ox_lib'
-description 'A library of shared functions to utilise in other resources.'
+name 'template mantine'
+author 'Arius'
 
-dependencies {
-    '/server:7290',
-    '/onesync',
-}
+
 
 ui_page 'web/build/index.html'
 
 files {
-
     'web/build/index.html',
     'web/build/**/*',
     'locales/*.json',
 }
-
 
 
 client_scripts {
